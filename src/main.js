763dc9c8421ac16677e97b7911a2e0a998b9dd75
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-09-08 21:09:56 -0500

    include pwa and i18n

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
+import './registerServiceWorker'
+import i18n from './i18n'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(i18n).use(store).use(router).mount('#app')
