93f4af5035c69541dcfb7fb9b27fc477ed73cca3
Tomas Obregon <tomasobregon@iMac-de-Tomas.lan>
2021-09-15 12:54:44 -0300

    modal done, missing styles and user info to send

--- src/modules/FeedbackButton/Views/FeedbackButton.vue
+++ src/modules/FeedbackButton/Views/FeedbackButton.vue
@@ -1,56 +1,58 @@
-<template class="container mx-auto">
+<template>
   <div class="container mx-auto">
-    <!-- Tabs Titles -->
+    <!-- modal-->
+    <transition name="fade">
+      <div class="modal-overlay" v-if="showModal"></div
+    ></transition>
+    <transition name="fade">
+      <div class="modal" v-if="showModal">
+        <div>
+          <Form @submit="sendReport" class=" text-center  container mx-auto">
+            <div class="my-4">
+              <p class="text-gray-600">Type of report</p>
+              <Field
+                type="text"
+                class=" border border-gray-300 py-2"
+                name="report"
+                rules="required"
+              />
+              <br />
+              <ErrorMessage class="text-red-400" name="report"></ErrorMessage>
+            </div>
+            <div class="my-4">
+              <p class="text-gray-600">Description</p>
+              <Field
+                type="text"
+                class=" border border-gray-300 py-2"
+                name="description"
+                rules="required"
+              />
+              <br />
+              <ErrorMessage
+                class="text-red-400"
+                name="description"
+              ></ErrorMessage>
+            </div>
+            <div className="flex justify-center my-10">
+              <input
+                type="submit"
+                class="bg-blue-800 p-3 text-white cursor-pointer"
+                value="Send"
+              />
+            </div>
+          </Form>
+        </div>
+        <button @click="showModal = false">cerrar modal</button>
+      </div></transition
+    >
 
-    <!-- Icon -->
-    <div class="flex justify-center mx-10 my-10">
-      <img src="@/assets/logoWF.png" id="icon" alt="User Icon" />
-    </div>
-
-    <!-- Login Form -->
-    <Form @submit="submitLogin" class=" text-center  container mx-auto">
-      <div class="my-4">
-        <p class="text-gray-600">Email</p>
-        <Field
-          type="email"
-          id="login"
-          class=" border border-gray-300 py-2"
-          v-model="usuario"
-          name="email"
-          rules="required"
-        />
-        <br />
-        <ErrorMessage class="text-red-400" name="email"></ErrorMessage>
-      </div>
-      <div class="my-4">
-        <p class="text-gray-600">Password</p>
-        <Field
-          type="password"
-          class=" border border-gray-300 py-2"
-          name="password"
-          rules="required"
-        />
-        <br />
-        <ErrorMessage class="text-red-400" name="password"></ErrorMessage>
-      </div>
-      <div className="flex justify-center my-10">
-        <input
-          type="submit"
-          class="bg-blue-800 p-3 text-white cursor-pointer"
-          value="Log In"
-        />
-      </div>
-    </Form>
-
-    <!-- Remind Passowrd -->
-    <div class="flex flex-col text-center">
-      <router-link class="text-blue-800 underline my-2" to="/register"
-        >Register</router-link
-      >
-      <router-link class="text-blue-800 underline my-2" to="/forgot-password"
-        >Forgot your Password?</router-link
-      >
-    </div>
+    <!-- boton para abrir modal-->
+    <button
+      class="bg-paleLogo rounded-full p-3 border shadow-lg border-border m-10"
+      @click="showModal = true"
+    >
+      🐞
+    </button>
   </div>
 </template>
 
@@ -58,25 +60,53 @@
 // @ is an alias to /src
 import axios from "axios";
 import { Form, Field, ErrorMessage } from "vee-validate";
+import { useRoute } from "vue-router";
+
 export default {
-  name: "Login",
+  name: "FeedbackButton",
   components: { Form, Field, ErrorMessage },
   data() {
-    return { usuario: "", password: "" };
+    const route = useRoute();
+    return {
+      showModal: false,
+      report: "",
+      description: "",
+      route: route.name,
+    };
   },
+  setup() {},
   methods: {
-    submitLogin(e, values) {
-      console.log(e, values);
-      // e.preventDefault();
-      if (!e.signDashboard) {
-        e.signDashboard = false;
-      }
-      let json = { usuario: this.usuario, password: this.password };
-      console.log(this.usuario);
-      axios.post("http://solodata.es/auth", json).then((data) => {
+    sendReport(e) {
+      e.path = this.route;
+      console.log(e);
+      axios.post("http://solodata.es/auth", e).then((data) => {
         console.log(data);
       });
     },
   },
 };
 </script>
+
+<style>
+fade-enter {
+}
+
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.4);
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  z-index: 101;
+}
+</style>
