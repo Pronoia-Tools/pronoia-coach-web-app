d0f8bc2c9ce6097709b793c8ac76d2add5090f09
Tomas Obregon <tomasobregon@Air-de-Tomas.lan>
2021-09-30 21:25:54 -0300

    handling errors front back in login

--- src/modules/Auth/Store/actions.js
+++ src/modules/Auth/Store/actions.js
@@ -1,26 +1,26 @@
 import PronoiaAPI from "../../../api/PronoiaAPI";
 
-export const signUp = async ({commit}, data) => {
-  let response = await PronoiaAPI.post('/auth/register', data)
-  commit("signUp", response)
-  return true
-}
+export const signUp = async ({ commit }, data) => {
+  let response = await PronoiaAPI.post("/auth/register", data);
+  commit("signUp", response);
+  return true;
+};
 
-export const login = async ({commit}, data) => {
+export const login = async ({ commit }, data) => {
   try {
-    let response = await PronoiaAPI.post('/auth/login', data)
-    console.log({"[[ACTIONS {{login}} ]]":response})
-    commit("login", response.data)
-    return response.data
+    let response = await PronoiaAPI.post("/auth/login", data);
+    console.log({ "[[ACTIONS {{login}} ]]": response });
+    commit("login", response.data);
+    return response.status;
   } catch (error) {
-    console.log(error)
-    return null
+    if (error.response) {
+      return error.response.status;
+    } else return 500;
   }
-}
+};
 // export const saveWorkbook = async ({commit},workbook) =>{
 //   // console.log(commit,workbook)
 
-  
 //   const data = await new Promise(resolve => setTimeout(() => resolve(workbook), 1000));
 //   if(!data) return
 //   console.log(data)
