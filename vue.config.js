c3456854ae92d8d124208a53e3d199b4607d825f
Juan Pablo <jp.uruza.t@gmail.com>
2021-11-01 13:16:03 -0300

    page title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Pronoia Cloud'
+      return args
+    })
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -8,6 +16,6 @@ module.exports = {
       runtimeOnly: false,
       compositionOnly: false,
       fullInstall: true
-    }
+    },
   }
 }
