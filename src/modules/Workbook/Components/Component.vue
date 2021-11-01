f0f05cb4d33362d616b89ae49a77fc1400c0dd5c
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-11-01 15:06:54 -0600

    base resize

---
+++ src/modules/Workbook/Components/Component.vue
@@ -0,0 +1,61 @@
+<template>
+  <node-view-wrapper class="vue-component bg-red-200" >
+    <div class="container">
+      <div class="content">
+      <img :src="node.attrs.src">
+        <!-- <button @click="increase">
+          This button has been clicked {{ node.attrs.count }} times.
+        </button> -->
+        <span>{{node.attrs.width}}</span>
+      </div>
+    </div>
+  </node-view-wrapper>
+</template>
+
+<script>
+import { NodeViewWrapper, nodeViewProps } from '@tiptap/vue-3'
+
+export default {
+  components: {
+    NodeViewWrapper,
+  },
+
+  props: nodeViewProps,
+
+  methods: {
+  },
+  computed:{
+    getSize(){
+      return `${this.node.attrs.width}px`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.vue-component {
+  // background: #FAF594;
+  border: 3px solid #0D0D0D;
+  border-radius: 0.5rem;
+  margin: 1rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+.container{
+  position: relative;
+  resize: both;
+  // padding: 20px; 
+  // width: 300px;
+  overflow: auto;
+  border: 1px solid black;
+}
+.content {
+  // margin-top: 1.5rem;
+  // padding: 1rem;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
--- src/modules/Workbook/Helpers/Image.js
+++ src/modules/Workbook/Helpers/Image.js
@@ -1,71 +1,51 @@
-'use strict';
+import { Node, mergeAttributes } from '@tiptap/core'
+import { VueNodeViewRenderer } from '@tiptap/vue-3'
+import Component from "../Components/Component.vue"
 
-Object.defineProperty(exports, '__esModule', { value: true });
+export default Node.create({
+    name: 'vueComponent',
 
-var core = require('@tiptap/core');
+    group: 'block',
 
-const inputRegex = /!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\)/;
-const Image = core.Node.create({
-    name: 'image',
-    defaultOptions: {
-        inline: false,
-        HTMLAttributes: {},
-    },
-    inline() {
-        return this.options.inline;
-    },
-    group() {
-        return this.options.inline ? 'inline' : 'block';
-    },
-    draggable: true,
+    atom: true,
+
     addAttributes() {
         return {
-            src: {
-                default: null,
-            },
-            alt: {
-                default: null,
-            },
-            title: {
-                default: null,
-            },
-            class:{
-              default:null,
-            }
-        };
+        src: {
+            default: "https://static.remove.bg/remove-bg-web/194d453110e760e94498dbb94c5cfb329903342c/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png",
+        },
+        width:{
+            default:300
+        }
+        }
     },
+
     parseHTML() {
         return [
-            {
-                tag: 'img[src]',
-            },
-        ];
+        {
+            tag: 'vue-component',
+        },
+        ]
     },
+
     renderHTML({ HTMLAttributes }) {
-        return ['img', core.mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)];
+        return ['vue-component', mergeAttributes(HTMLAttributes)]
+    },
+
+    addNodeView() {
+        return VueNodeViewRenderer(Component)
     },
     addCommands() {
         return {
             setImage: options => ({ commands }) => {
-              console.log(options)
+                console.log(options)
                 return commands.insertContent({
                     type: this.name,
                     attrs: options,
                 });
+                // console.log(options)
+                // return commands.setNode('vueComponent')
             },
         };
     },
-    addInputRules() {
-        return [
-            core.nodeInputRule(inputRegex, this.type, match => {
-                const [, alt, src, title] = match;
-                return { src, alt, title };
-            }),
-        ];
-    },
-});
-
-exports.Image = Image;
-exports.default = Image;
-exports.inputRegex = inputRegex;
-//# sourceMappingURL=tiptap-extension-image.cjs.js.map
+})
