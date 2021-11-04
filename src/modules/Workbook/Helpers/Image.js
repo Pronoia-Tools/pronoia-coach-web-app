8e64c7db8c1379113ee4cc3af2f5b327a672eafe
Victor Manuel Delfin Santos <79028635+ByVictor28@users.noreply.github.com>
2021-11-04 07:06:40 -0600

    Resize and align image (#62)
    * base resize
    
    * custom resisze completed
    
    * resize and align image completed
    
    * change resize icon

--- src/modules/Workbook/Helpers/Image.js
+++ src/modules/Workbook/Helpers/Image.js
@@ -1,71 +1,54 @@
-'use strict';
+import { Node, mergeAttributes } from '@tiptap/core'
+import { VueNodeViewRenderer } from '@tiptap/vue-3'
+import TipTapResizebleImage from "../Components/TipTapResizebleImage.vue"
 
-Object.defineProperty(exports, '__esModule', { value: true });
+export default Node.create({
+    name: 'vueComponent',
 
-var core = require('@tiptap/core');
+    group: 'block',
 
-const inputRegex = /!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\)/;
-const Image = core.Node.create({
-    name: 'image',
-    defaultOptions: {
-        inline: false,
-        HTMLAttributes: {},
-    },
-    inline() {
-        return this.options.inline;
-    },
-    group() {
-        return this.options.inline ? 'inline' : 'block';
-    },
-    draggable: true,
+    atom: true,
+
     addAttributes() {
         return {
             src: {
-                default: null,
+                default: "https://static.remove.bg/remove-bg-web/194d453110e760e94498dbb94c5cfb329903342c/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png",
             },
-            alt: {
-                default: null,
+            width:{
+                default:100
             },
-            title: {
-                default: null,
-            },
-            class:{
-              default:null,
+            justify:{
+                default:"center"
             }
-        };
+        }
     },
+
     parseHTML() {
         return [
-            {
-                tag: 'img[src]',
-            },
-        ];
+        {
+            tag: 'vue-component-resizeble-Image',
+        },
+        ]
     },
+
     renderHTML({ HTMLAttributes }) {
-        return ['img', core.mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)];
+        return ['vue-component-resizeble-Image', mergeAttributes(HTMLAttributes)]
+    },
+
+    addNodeView() {
+        return VueNodeViewRenderer(TipTapResizebleImage)
     },
     addCommands() {
         return {
             setImage: options => ({ commands }) => {
-              console.log(options)
+                console.log(options)
                 return commands.insertContent({
                     type: this.name,
                     attrs: options,
                 });
+                // console.log(options)
+                // return commands.setNode('vueComponent')
             },
         };
     },
-    addInputRules() {
-        return [
-            core.nodeInputRule(inputRegex, this.type, match => {
-                const [, alt, src, title] = match;
-                return { src, alt, title };
-            }),
-        ];
-    },
-});
-
-exports.Image = Image;
-exports.default = Image;
-exports.inputRegex = inputRegex;
-//# sourceMappingURL=tiptap-extension-image.cjs.js.map
+})
