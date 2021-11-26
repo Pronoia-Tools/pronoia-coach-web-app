b638f8eb59a80509a7e76701f4675ba83edc8145
IamCocoDev <giovencofede@gmail.com>
2021-11-26 11:56:43 -0300

    library and search and preview finished

--- src/modules/Workbook/Views/WorkBooksLibrary.vue
+++ src/modules/Workbook/Views/WorkBooksLibrary.vue
@@ -1,16 +1,37 @@
 <template>
 	<div class="flex flex-col w-full h-full items-center">
-    <div class="m-4">
-      <input placeholder="Buscar" type="text" name="" id="">
-    </div>
+		<div class="m-4">
+			<input
+				@keydown="onChange"
+				v-model="inputWB"
+				placeholder="Buscar"
+				type="text"
+				name=""
+				id=""
+			/>
+		</div>
 		<div class="flex flex-col W-full justify-center items-center m-3">
 			<h1>Your workbooks</h1>
-      <div class="flex flex-row items-center justify-center">
-        <div class=" m-3 w-1/6 flex flex-col items-center" v-for="workbook in wokbookFilter" :key="workbook.id">
-          <img :src="workbook.image" alt="" @click="gotopreview(workbook.id)" />
-          <h2>{{ workbook.title }}</h2>
+				<div v-if="inputWB === null" class="flex flex-row items-center justify-center" >
+          <div
+            class=" m-3 w-1/6 flex flex-col items-center"
+            v-for="workbook in allWBS"
+            :key="workbook.id"
+          >
+            <img :src="workbook.image" alt="" @click="gotopreview(workbook.id)" />
+            <h2>{{ workbook.title }}</h2>
+          </div>
         </div>
-      </div>
+        <div v-if="inputWB !== null" class="flex flex-row items-center justify-center" >
+          <div
+            class=" m-3 w-1/6 flex flex-col items-center"
+            v-for="workbook in wokbookFilter"
+            :key="workbook.id"
+          >
+            <img :src="workbook.image" alt="" @click="gotopreview(workbook.id)" />
+            <h2>{{ workbook.title }}</h2>
+          </div>
+			</div>
 		</div>
 		<div>
 			<h1>Shared workbooks</h1>
@@ -27,8 +48,10 @@
 		/*  components: { Spiner }, */
 		data() {
 			return {
-        wokbookFilter: null
-      };
+				wokbookFilter: null,
+				inputWB: null,
+				allWBS: null,
+			};
 		},
 		computed: {
 			...mapGetters("workBook", ["getWorkBooks", "getLoading"]),
@@ -41,13 +64,22 @@
 				});
 				window.open(route.href);
 			},
-      workBooksList() {
-				this.wokbookFilter = this.getWorkBooks;
+			workBooksList() {
+				this.allWBS = this.getWorkBooks;
+				console.log(this.allWBS);
+			},
+			onChange() {
+        if (this.inputWB === '') {
+          this.wokbookFilter = null
+        }
+				this.wokbookFilter = this.allWBS.filter((e) =>
+					e.title.includes(this.inputWB.toLowerCase()),
+				);
 			},
 		},
 		created() {
-      this.workBooksList()
-    },
+			this.workBooksList();
+		},
 	};
 </script>
 
