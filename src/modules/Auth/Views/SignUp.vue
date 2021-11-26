16a8c656235506537684fdb200c85b20b22a0640
Juan Pablo Urzua <jp.urzua.t@gmail.com>
2021-11-25 22:14:10 -0300

    udpates (#95)
    * udpates
    
    * Stripe for alpha and prod
    
    Co-authored-by: Juan Pablo <jp.uruza.t@gmail.com>

--- src/modules/Auth/Views/SignUp.vue
+++ src/modules/Auth/Views/SignUp.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="w-full flex flex-col items-center">
-    <div class=" w-10/12 md:w-6/12 lg:w-4/12 py-10">
+  <div class="w-full flex flex-col aling-center items-center">
+    <div class="flex w-10/12 md:w-6/12 lg:w-4/12 py-10 place-content-center">
+      <!-- image usign Coach_Portfolios -->
       <img src="@/assets/WorkbookFactoryLogo.png" alt="logo" />
-
+    </div>
+    <div class="flex flex-row place-content-center">
+    <div class="flex w-10/12 md:w-6/12 lg:w-4/12 py-10 place-content-center">
+      <!-- image usign Coach_Portfolios -->
+      <img src="@/assets/Coach_Portfolios.png" alt="logo" />
+    </div>
+    <div class="w-10/12 md:w-6/12 lg:w-4/12 py-10">
       <Form
         class="text-gray-500 px-10 flex flex-col items-center gap-2"
         @submit="submitSignUp"
       >
-        <div class=" grid grid-cols-2 gap-4 w-full">
+        <div class="grid grid-cols-2 gap-4 w-full">
           <div class="field">
-            <label class="block" for="fistName">{{$t("sign-up.first-name")}}</label>
+            <label class="block" for="fistName">{{
+              $t("sign-up.first-name")
+            }}</label>
             <Field
               class="w-full px-2 py-1 border rounded border-gray-400"
               type="text"
@@ -20,7 +29,9 @@
           </div>
 
           <div class="field">
-            <label class="block" for="LastName">{{$t("sign-up.last-name")}}</label>
+            <label class="block" for="LastName">{{
+              $t("sign-up.last-name")
+            }}</label>
             <Field
               class="w-full px-2 py-1 border rounded border-gray-400"
               type="text"
@@ -32,7 +43,7 @@
         </div>
 
         <div class="field w-full">
-          <label class="block" for="email">{{$t("sign-up.email")}}</label>
+          <label class="block" for="email">{{ $t("sign-up.email") }}</label>
           <Field
             class="w-full px-2 py-1 border rounded border-gray-400"
             type="email"
@@ -43,7 +54,9 @@
         </div>
 
         <div class="field w-full">
-          <label class="block" for="password">{{$t("sign-up.password")}}</label>
+          <label class="block" for="password">{{
+            $t("sign-up.password")
+          }}</label>
           <Field
             class="w-full px-2 py-1 border rounded border-gray-400"
             type="password"
@@ -54,9 +67,9 @@
         </div>
 
         <div class="field w-full">
-          <label class="block" for="passwordConfirm"
-            >{{$t("sign-up.password-confirmation")}}</label
-          >
+          <label class="block" for="passwordConfirm">{{
+            $t("sign-up.password-confirmation")
+          }}</label>
           <Field
             class="w-full px-2 py-1 border rounded border-gray-400"
             type="password"
@@ -70,15 +83,21 @@
         </div>
 
         <div class="field w-full">
-          <label class="block" for="country">{{$t("sign-up.country")}}</label>
+          <label class="block" for="country">{{ $t("sign-up.country") }}</label>
           <Field
             class="w-full px-2 py-1 border rounded border-gray-400"
             name="country"
             rules="required"
             as="select"
           >
-            <option value="" disabled>{{$t("input.select")}}</option>
-            <option v-for="country in countryList" :key="country.countryCode" :value="country.name">{{country.name}}</option>
+            <option value="" disabled>{{ $t("input.select") }}</option>
+            <option
+              v-for="country in countryList"
+              :key="country.countryCode"
+              :value="country.name"
+            >
+              {{ country.name }}
+            </option>
             <!-- <option value="Mexico">{{$t("countries.mexico")}}</option>
             <option value="USA">{{$t("countries.usa")}}</option> -->
           </Field>
@@ -86,15 +105,23 @@
         </div>
 
         <div class="field w-full">
-          <label class="block" for="listing_badge">{{$t("sign-up.listing_badge")}}</label>
+          <label class="block" for="listing_badge">{{
+            $t("sign-up.listing_badge")
+          }}</label>
           <Field
             class="w-full px-2 py-1 border rounded border-gray-400"
             name="listing_badge"
             rules="required"
             as="select"
           >
-            <option value="" disabled>{{$t("input.select")}}</option>
-            <option v-for="badge in badgeList" :key="badge.id" :value="badge.name">{{badge.name}}</option>
+            <option value="" disabled>{{ $t("input.select") }}</option>
+            <option
+              v-for="badge in badgeList"
+              :key="badge.id"
+              :value="badge.name"
+            >
+              {{ badge.name }}
+            </option>
             <!-- <option value="Mexico">{{$t("countries.mexico")}}</option>
             <option value="USA">{{$t("countries.usa")}}</option> -->
           </Field>
@@ -102,35 +129,36 @@
         </div>
 
         <div class="flex flex-col justify-start">
+          <div class="flex justify-start items-center gap-3 my-4">
+            <Field
+              type="checkbox"
+              name="newsletter"
+              class="block"
+              :value="true"
+            />
+            <label class="block text-blue-500" for="cbox2">{{
+              $t("sign-up.newsletter_checkbox")
+            }}</label>
+          </div>
 
-      <div class="flex justify-start items-center gap-3 my-4">
-          <Field
-            type="checkbox"
-            name="newsletter"
-            class="block"
-            :value="true"
-          />
-          <label class="block text-blue-500" for="cbox2"
-            >{{$t("sign-up.newsletter_checkbox")}}</label
-          >
-        </div>
+          <div class="flex justify-start items-start gap-3 my-4">
+            <Field
+              type="checkbox"
+              name="pre_launch"
+              class="block"
+              :value="true"
+            />
+            <label class="block text-blue-500" for="cbox2">{{
+              $t("sign-up.pre_launch")
+            }}</label>
+            <span
+              title="Lock-in your first year of the full Pronoia Cloud suite with this exclusive pre-launch pricing. $35 today locks in your first year at that rate. After the launch Pronoia Cloud will be $50/month"
+            >
+              <FontAwesomeIcon :icon="InfoCircle"></FontAwesomeIcon>
+            </span>
+          </div>
 
-        <div class="flex justify-start items-start gap-3 my-4">
-          <Field
-            type="checkbox"
-            name="pre_launch"
-            class="block"
-            :value="true"
-          />
-          <label class="block text-blue-500" for="cbox2"
-            >{{$t("sign-up.pre_launch")}}</label
-          >
-          <span title="Lock-in your first year of the full Pronoia Cloud suite with this exclusive pre-launch pricing. $35 today locks in your first year at that rate. After the launch Pronoia Cloud will be $50/month">
-                <FontAwesomeIcon :icon="InfoCircle"></FontAwesomeIcon>
-              </span>
-        </div>
-        
-        <!-- <div class="flex justify-start items-center gap-3 my-4">
+          <!-- <div class="flex justify-start items-center gap-3 my-4">
           <Field
             type="checkbox"
             name="notifyme"
@@ -143,27 +171,35 @@
         </div> -->
         </div>
         <div>
-          <button class="p-2 bg-purple-900 text-white" >{{$t("sign-up.submit")}}</button>
+          <button class="p-2 bg-purple-900 text-white">
+            {{ $t("sign-up.submit") }}
+          </button>
         </div>
 
-        <router-link to="/login" class="underline text-purple-900">{{$t("sign-up.already-have-acount")}}</router-link>
+        <router-link to="/login" class="underline text-purple-900">{{
+          $t("sign-up.already-have-acount")
+        }}</router-link>
         <!-- <ButoomCustomVue class="bg-myOrange" @click="toogleShowModalPayment">{{$t("input.joinWhitelist")}}</ButoomCustomVue> -->
       </Form>
     </div>
+    </div>
   </div>
-  <PaymentModalVue :showModal="showModalPayment" @toogleShowModal="toogleShowModalPayment"></PaymentModalVue>
+  <PaymentModalVue
+    :showModal="showModalPayment"
+    @toogleShowModal="toogleShowModalPayment"
+  ></PaymentModalVue>
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
-import { Form, Field, ErrorMessage } from "vee-validate"
-import { mapActions, mapState } from 'vuex';
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { mapActions, mapState } from "vuex";
 import Swall from "sweetalert2";
-import countries from "@/assets/countryList.json"
-import badges from "../../../utils/badgeList"
-import PaymentModalVue from '../../../components/PaymentModal.vue';
+import countries from "@/assets/countryList.json";
+import badges from "../../../utils/badgeList";
+import PaymentModalVue from "../../../components/PaymentModal.vue";
 // import ButoomCustomVue from '../../../components/ButoomCustom.vue';
 
 export default {
@@ -175,54 +211,54 @@ export default {
     FontAwesomeIcon,
     // ButoomCustomVue,
   },
-  data(){
-    return{
-      countryList:countries,
+  data() {
+    return {
+      countryList: countries,
       badgeList: badges,
-      showModalPayment:false,
-      InfoCircle:faInfoCircle,
-    }
+      showModalPayment: false,
+      InfoCircle: faInfoCircle,
+    };
   },
   computed: {
     ...mapState("auth", ["isAuthenticated"]),
   },
-  methods:{
-    ...mapActions("auth",["signUp", "logout"]),
-    toogleShowModalPayment(){
-      this.showModalPayment =!this.showModalPayment
+  methods: {
+    ...mapActions("auth", ["signUp", "logout"]),
+    toogleShowModalPayment() {
+      this.showModalPayment = !this.showModalPayment;
     },
-    async submitSignUp(data){
+    async submitSignUp(data) {
       let revisedData = {
         firstname: data.firstName,
         lastname: data.LastName,
         email: data.email,
         password: data.password,
         country: data.country,
-        notify:data.notifyme,
+        notify: data.notifyme,
         listing_badge: data.listing_badge,
         newsletter: data.newsletter,
         pre_launch: data.pre_launch,
       };
       if (!revisedData.notify) {
-        revisedData.notify=false
+        revisedData.notify = false;
       }
       if (!revisedData.pre_launch) {
-        revisedData.pre_launch=false
+        revisedData.pre_launch = false;
       }
       if (!revisedData.newsletter) {
-        revisedData.newsletter=false
+        revisedData.newsletter = false;
       }
-      console.log(revisedData)
+      console.log(revisedData);
       try {
         const response = await this.signUp(revisedData);
         if (this.isAuthenticated) {
-          console.log("success")
-          console.log(response)
-          console.log(response.data)
+          console.log("success");
+          console.log(response);
+          console.log(response.data);
           // console.log(response.data.data)
           // console.log(response.data.data.token)
           // console.log(response.data.data.user)
-          
+
           // search in data.badgeList for object with name revisedData.listing_badge
           // let badge = this.badgeList.find(badge => badge.name === revisedData.listing_badge);
           // const listItems = []
@@ -233,7 +269,7 @@ export default {
           //       quantity: 1,
           //     });
           //   }
-           
+
           //   if (revisedData.pre_launch) {
           //     listItems.push({
           //       price: process.env.VUE_APP_STRIPE_PRE_LAUNCH,
@@ -256,40 +292,38 @@ export default {
           //   }
           // this.showModalPayment =!this.showModalPayment;
           // this.$router.push({name:"WorkBookLayout"});
-          console.log('dsasd')
+          console.log("dsasd");
           if (response.data.stripe_session) {
-            console.log("stripe session")
+            console.log("stripe session");
             this.stripe.redirectToCheckout({
-              sessionId: response.data.stripe_session.id
+              sessionId: response.data.stripe_session.id,
             });
           } else {
             this.logout();
           }
         } else {
           Swall.fire({
-            title:this.$t("swallAlertGeneral.error"),
-            text:this.$t("swallAlertGeneral.try-again"),
-            icon:"error"
+            title: this.$t("swallAlertGeneral.error"),
+            text: this.$t("swallAlertGeneral.try-again"),
+            icon: "error",
           });
         }
-        
       } catch (error) {
         Swall.fire({
-          icon:"error",
+          icon: "error",
           title: `${this.$t("swallAlertGeneral.error")}`,
-          text:error.response.data.message
-        })
+          text: error.response.data.message,
+        });
       }
     },
-    loadStripe(){
-      console.log("loadStripe")
+    loadStripe() {
+      console.log("loadStripe");
       /* global Stripe */
       this.stripe = Stripe(process.env.VUE_APP_STRIPE);
     },
-    
   },
-  mounted(){  
-    this.loadStripe()
+  mounted() {
+    this.loadStripe();
   },
-}
+};
 </script>
--- src/modules/Auth/Views/Success.vue
+++ src/modules/Auth/Views/Success.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="w-full flex flex-col items-center">
-    <div class="w-10/12 md:w-6/12 lg:w-4/12 py-10">
+<div class="w-full flex flex-col aling-center items-center">
+    <div class="flex w-10/12 md:w-6/12 lg:w-4/12 py-10 place-content-center">
+      <!-- image usign Coach_Portfolios -->
       <img src="@/assets/WorkbookFactoryLogo.png" alt="logo" />
-
-      <h1> Thank you! </h1>
+    </div>
+    <div class="flex flex-row place-content-center">
+    <div class="flex w-10/12 md:w-6/12 lg:w-4/12 py-10 place-content-center">
+      <!-- image usign Coach_Portfolios -->
+      <img src="@/assets/Coach_Portfolios.png" alt="logo" />
+    </div>
+    <div class="w-10/12 md:w-6/12 lg:w-4/12 py-10">
+  <div class="text-gray-500 px-10 flex flex-col items-center gap-2">
+    <h1> Thank you! </h1>
+    <span> You have successfully registered. We will be emailing you with details!</span>
+  </div>
+      
+    </div>
     </div>
   </div>
 </template>
@@ -21,11 +33,9 @@ export default {
     // ErrorMessage,
     // ButoomCustomVue
   },
-  methods:{
-  }
-}
+  methods: {},
+};
 </script>
 
 <style>
-
 </style>
