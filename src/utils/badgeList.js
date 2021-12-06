02a6acfa604d68f364fbf0548d2043f8ff60d151
Juan Pablo Urzua <jp.urzua.t@gmail.com>
2021-12-06 15:40:13 -0300

    env example (#103)
    * env example
    
    * env variables + code cleanup
    
    Co-authored-by: Juan Pablo <jp.uruza.t@gmail.com>

--- src/utils/badgeList.js
+++ src/utils/badgeList.js
@@ -3,25 +3,21 @@ export default [
       "id": 0,
       "name": "Co-Creator Listing Badge",
       "price": 0,
-      "stripe_price": ""
     },
     {
       "id": 1,
       "name": "$50 Diamond Co-Creator Listing Badge",
       "price": 50,
-      "stripe_price": process.env.VUE_APP_STRIPE_1
     },
     {
       "id": 2,
       "name": "$40 Gold Co-Creator Listing Badge",
       "price": 40,
-      "stripe_price": process.env.VUE_APP_STRIPE_2
     },
     {
       "id": 3,
       "name": "$25 Silver Co-Creator Listing Badge",
       "price": 25,
-      "stripe_price": process.env.VUE_APP_STRIPE_3
     }
   
 ];
